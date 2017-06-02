67bb121c3b29681ceef35a8f60b888bcd643c9cb
noraworld <noraworld@mail.com>
2017-06-03 04:12:40 +0900

    :bug: Fix some bugs
    - Content warning words are not counted in word count
    
    - Tootting by pressing super key and enter key even if no focus on text area
    
    - Broken layout when showing a message that a user should auth this app

--- src/js/popup.js
+++ src/js/popup.js
@@ -21,29 +21,45 @@
   });
 
   function init() {
-    validateCount();
     count();
+    validateCount();
 
     document.querySelector("#toot").addEventListener("click", toot);
     document.querySelector("#cw").addEventListener("change", toggleSpoilerField);
     document.addEventListener("keyup", function(event) {
       count();
       validateCount();
+      saveDraft();
     });
     document.addEventListener("keydown", function(event) {
-      if (((event.ctrlKey && !event.metaKey) || (event.metaKey && !event.ctrlKey)) && event.key === 'Enter' && validateCount()) {
+      if (((event.ctrlKey && !event.metaKey) || (event.metaKey && !event.ctrlKey)) && event.key === 'Enter' && validateCount() && validateFocus()) {
         toot();
       }
-      saveDraft();
       refleshStatus();
     });
   }
 
   function count() {
     let content = document.querySelector("#content").value;
-    let left = 500 - content.length;
+    let spoiler = document.querySelector("#spoiler-text").value;
+    let left = 500 - (content.length + spoiler.length);
 
     document.querySelector("#words").textContent = left;
+  }
+
+  function validateCount() {
+    let content = document.querySelector("#content");
+    let spoiler = document.querySelector("#spoiler-text");
+    let count = content.value.length + spoiler.value.length;
+
+    if (count <= 0 || count > 500) {
+      disableTootButton();
+      return false;
+    }
+    else {
+      enableTootButton();
+      return true;
+    }
   }
 
   function toot() {
@@ -81,10 +97,10 @@
         status.classList.remove("text-success");
         status.classList.add("text-danger");
         if (settings.token.access_token) {
-          status.textContent = "Toot failed.";
+          status.textContent = "Toot failed";
         }
         else {
-          status.innerHTML = 'You need to auth this app from <a href="/options.html" target="_blank">options page</a>';
+          status.innerHTML = 'Auth this app from <a href="/options.html" target="_blank">options page</a>';
         }
       }
     }
@@ -106,15 +122,16 @@
     });
   }
 
-  function validateCount() {
-    let content = document.querySelector("#content")
-    if (content.value.length <= 0 || content.value.length > 500) {
-      disableTootButton();
-      return false;
+  function validateFocus() {
+    let active = document.activeElement;
+    let content = document.querySelector("#content");
+    let spoiler = document.querySelector("#spoiler-text");
+
+    if (active === content || active === spoiler) {
+      return true;
     }
     else {
-      enableTootButton();
-      return true;
+      return false;
     }
   }
 
@@ -135,17 +152,27 @@
   }
 
   function hideSpoiler() {
+    document.querySelector("#spoiler-text").value = "";
+    count();
+    validateCount();
     document.querySelector("#spoiler-text").style.display = "none";
   }
 
   function disableTootButton() {
     let tootButton = document.querySelector("#toot");
     tootButton.setAttribute("disabled", "");
+    let words = document.querySelector("#words");
+
+    if (words.textContent !== "500") {
+      words.style.color = "#d9534f";
+    }
   }
 
   function enableTootButton() {
     let tootButton = document.querySelector("#toot");
     tootButton.removeAttribute("disabled");
+    let words = document.querySelector("#words");
+    words.style.color = "";
   }
 
   function refleshStatus() {
